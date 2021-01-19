034ca57cd3f51e34908421e35ba554271eff1f86
LiterallyJohnny <liljohnnyrayy+git@gmail.com>
2021-01-19 13:51:42 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# HydraBot
+
+## About Hydra
+Hydra is a multipurpose Discord bot written in Python. It can do many things, such as kicking/banning people to Reddit modmail-like features.
---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+import os
+from dotenv import load_dotenv
+import logging
+
+
+# Enables logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+# Assigns the Discord client to a variable called client
+bot = discord.Client()
+
+
+# Loads .env
+load_dotenv()
+
+
+# Prints a message stating that the bot is loggen in
+@bot.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot))
+
+# Prints the help message when a user says $help
+@bot.event
+async def on_message(message):
+    if message.content.startswith('$help'):
+        await message.channel.send('''**Hydra Help**
+*This message contains a list of commands and what they do.*
+
+__help__: Prints this message.''')
+
+
+# Starts the bot
+bot.run(os.getenv('TOKEN'))
