caaf5eaeaeaa636b8860307d8b031719b8897975
Johnny Nalley <liljohnnyrayy@gmail.com>
2021-03-11 13:02:27 -0600

    Laid out the frameworks of the bot

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import discord
+import os
 
-from client_token import token
+from discord.ext import commands
+from dotenv import load_dotenv
 
-client = discord.Client()
+load_dotenv()
+
+command_prefix = '$'
+description = f'''Hydra Commands
+Hydra's command prefix is "{command_prefix}"
+'''
+
+# bot = commands.Bot(command_prefix=command_prefix, description=description)
+bot = commands.Bot(command_prefix=command_prefix, description=description)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print(f'Logged in as {bot.user.name}')
+    print(f'{bot.user.name}\'s user ID is {bot.user.id}')
+    print('----------')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-
-# Before trying to run the bot, make sure your create a file called client_token.py in the same directory as this
-# file, and create a variable called "token" which contains the bot token.
-client.run(token)
+# Before trying to run the bot, make sure your create a file called .env in the same directory as this
+# file, and assign your token to TOKEN in .env (Ex. TOKEN='token here')
+bot.run(os.getenv('TOKEN'))
