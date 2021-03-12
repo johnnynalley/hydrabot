8253bd7bc9365cb67c1d50710038235e56422cca
Johnny Nalley <liljohnnyrayy@gmail.com>
2021-03-12 10:47:14 -0600

    Added settings

---
+++ hydra/settings.py
@@ -0,0 +1,15 @@
+import discord
+
+# Welcomer settings
+welcomeChannel = "819624923445985310"
+
+# Bot intents settings
+intents = discord.Intents(members=True)
+
+# Command prefix
+command_prefix = '$'
+
+# Bot help page
+description = f'''Hydra Commands
+Hydra's command prefix is "{command_prefix}"
+'''
