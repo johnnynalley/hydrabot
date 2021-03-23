432d99d14833c085b5f62b588a719678f5efac5f
Johnny Nalley <liljohnnyrayy@gmail.com>
2021-03-23 11:28:36 -0500

    Added extra information into the welcome message

--- hydra/settings.py
+++ hydra/settings.py
@@ -12,4 +12,12 @@ Hydra's command prefix is "{command_prefix}"
 '''
 
 # Welcome message
-welcomeMessage = 'Welcome to Code with Us!'
+welcomeMessage = '''
+**Welcome to Code with Us!**
+Code with Us is a programming community focused on coding, but we also have things that may be of interest to those who do not code.
+
+We have numerous channels for many different programming languages where you can look for help and discuss that language in. In the pinned messages of those channels, we have tips and tricks for programming with that language and resources for learning that language for those who don't already know it.
+
+We also have a custom bot named Hydra, built by LiterallyJohnny, the owner of this server. It is still in development, so don't expect too much from it.
+You can find the commands for Hydra by typing `$help` in <#819624945725210645>.
+'''
