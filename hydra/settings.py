54674f206e814070a7896663fe4cbd33aa2ce2e6
Johnny Nalley <liljohnnyrayy@gmail.com>
2021-03-29 08:02:52 -0500

    Made a few small changes to the welcome message

--- hydra/settings.py
+++ hydra/settings.py
@@ -9,7 +9,7 @@ command_prefix = '$'
 
 # Bot help page
 description = f'''Hydra Commands
-Hydra's command prefix is "{command_prefix}"
+Hydra's command prefix is "{command_prefix}."
 '''
 
 # Welcome message
@@ -19,6 +19,8 @@ Code with Us is a programming community focused on coding, but we also have thin
 
 We have numerous channels for many different programming languages where you can look for help and discuss that language in. In the pinned messages of those channels, we have tips and tricks for programming with that language and resources for learning that language for those who don't already know it.
 
-We also have a custom bot named Hydra, built by LiterallyJohnny, the owner of this server. It is still in development, so don't expect too much from it.
+We also have a custom bot named **Hydra**, built by **LiterallyJohnny**, the owner of this server. It is still in development, so don't expect too much from it.
 You can find the commands for Hydra by typing `$help` in <#819624945725210645>.
+
+*Thanks for joining **Code with Us**!*
 '''
